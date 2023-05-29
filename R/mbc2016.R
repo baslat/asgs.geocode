7417ec2f5dce136ab4f01cec28bac89fb85252bb
Bas Latcham <bas@latcham.au>
2023-05-29 12:44:06 +1000

    feat: :sparkles: Add `mbc2016` dataset

---
+++ R/mbc2016.R
@@ -0,0 +1,17 @@
+#' Meshblock Counts 2016
+#'
+#' Meshblock counts from the 2016 Census of Population and Housing, including
+#' category and counts of dwellings and persons by usual residence.
+#'
+#' @format ## `mbc2016`
+#' A data frame with 358,127 rows and 6 variables:
+#' \describe{
+#'  \item{mb_code_2016}{Meshblock code}
+#'  \item{mb_category}{Meshblock category}
+#'  \item{area_albers_sqkm}{Area in square kilometres}
+#'  \item{dwellings}{Count of dwellings}
+#'  \item{persons}{Count of persons usually resident}
+#'  \item{state_code}{State code}
+#' }
+#' @source [ABS](https://www.abs.gov.au/AUSSTATS/abs@.nsf/allprimarymainfeatures/B58F9B40B7D4B4CDCA257AD0000F1EF0?opendocument=)
+"mbc2016"
