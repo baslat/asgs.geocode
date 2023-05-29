6d58d9323b2e163fad7f3b01984dbd9562b05cd0
Bas Latcham <bas@latcham.au>
2023-05-29 15:30:36 +1000

    feat: :sparkles: Add spatial filter to `point_to_asgs()`

--- R/point_to_asgs.r
+++ R/point_to_asgs.r
@@ -13,5 +13,9 @@ point_to_asgs <- function(.data, layer) {
 	server <- "https://geo.abs.gov.au/arcgis/rest/services/ASGS2016/SEARCH/MapServer/"
 	url <- paste0(server, layer)
 	bbox <- sf::st_bbox(.data)
-	esri2sf::esri2sf(url, bbox = bbox)
+	esri2sf::esri2sf(url, bbox = bbox) |>
+		# Spatial filter to get closer to the initial geometry (ie not its bbox)
+		sf::st_filter(sf::st_transform(.data, crs = 4326L)) |>
+		# Rename all columns to lower
+		dplyr::rename_all(tolower)
 }
