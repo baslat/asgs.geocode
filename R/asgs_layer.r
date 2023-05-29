5f7efdb37563af722be99072dd9aafd1363251df
Bas Latcham <bas@latcham.au>
2023-05-29 15:09:19 +1000

    refactor: :recycle: Be more explicit in `asgs_layer()`

--- R/asgs_layer.r
+++ R/asgs_layer.r
@@ -1,12 +1,12 @@
 #' Return the ASGS layer ID
 #'
 #' @param year The year of the ASGS
-#' @param geo The geographic level (eg sa1, SUA)
+#' @param geo The geographic level (eg "sa1", "SUA")
 #' @return The ID of the layer
 #' @export
 asgs_layer <- function(year, geo) {
 	wanted <- paste("ASGS", year, toupper(geo), "GEN", sep = "_")
 	get_layers() |>
-		dplyr::filter(name == wanted) |>
-		dplyr::pull(id)
+		dplyr::filter(.data[["name"]] == wanted) |>
+		dplyr::pull(dplyr::all_of("id"))
 }
