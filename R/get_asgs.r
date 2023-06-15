de9e9e8f94a840f2350e956bb3cb225c081909b3
Bas Latcham <bas@latcham.au>
2023-06-16 08:51:00 +1000

    fix: :rotating_light: Fix lints

--- R/get_asgs.r
+++ R/get_asgs.r
@@ -1,6 +1,7 @@
-#' Geocode a point to ASGS
+#' Get the ASGS of an `sf` object
 #'
-#' This function geocodes an [sf::sf()] object to the Australian Statistical Geography Standardf.
+#' This function geocodes an [sf::sf()] object to the Australian Statistical
+#' Geography Standard.
 #'
 #' @param .data (sf) The input data to geocode to ASGS
 #' @inheritParams asgs_layer
@@ -22,21 +23,24 @@ get_asgs <- function(.data, geo, year) {
 		sf::st_filter(sf::st_transform(.data, crs = 4326L)) |>
 		# Rename all columns to lower
 		dplyr::rename_all(tolower)
-# Silence warning about spatially constant variables
-sf::st_agr(res_raw) <- "constant"
-sf::st_agr(.data) <- "constant"
-# Calculate the overlapping area
-overlaps <- res_raw |>
-	sf::st_intersection(.data) |>
-	dplyr::mutate(intersect_area = sf::st_area(geoms)) |>
-	sf::st_drop_geometry() |>
-	dplyr::summarise(
-		overlapping_area = as.numeric(sum(intersect_area)),
-		.by = objectid
-	)
 
-res_raw |>
-	dplyr::left_join(overlaps, by = dplyr::join_by(objectid)) |>
-	dplyr::mutate(overlapping_pct = overlapping_area / shape_area) |>
-	dplyr::relocate(overlapping_pct, .after = overlapping_area)
+ # Silence warning about spatially constant variables
+ sf::st_agr(res_raw) <- "constant"
+ sf::st_agr(.data) <- "constant"
+
+ # Calculate the overlapping area
+ # TODO is this meaningless when .data is a point inside the area?
+	overlaps <- res_raw |>
+		sf::st_intersection(.data) |>
+		dplyr::mutate(intersect_area = sf::st_area(.data[["geoms"]])) |>
+		sf::st_drop_geometry() |>
+		dplyr::summarise(
+			overlapping_area = as.numeric(sum(.data[["intersect_area"]])),
+			.by = .data[["objectid"]]
+		)
+
+	res_raw |>
+			dplyr::left_join(overlaps, by = "objectid") |>
+			dplyr::mutate(overlapping_pct = .data[["overlapping_area"]] / .data[["shape_area"]]) |>
+			dplyr::relocate(dplyr::all_of("overlapping_pct"), .after = dplyr::all_of("overlapping_area"))
 }
